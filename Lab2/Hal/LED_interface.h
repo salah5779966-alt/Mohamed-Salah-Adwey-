d60e220be584751870016fcadda69cf563bf8269
salah5779966-alt <salah5779966@gmail.com>
2026-09-06 13:54:09 +0300

    Delete Lab2 directory

--- Lab2/Hal/LED_interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef LED_INTERFACE_H_
-#define LED_INTERFACE_H_
-
-void LED_voidInit(u8 Copy_u8Port, u8 Copy_u8Pin);
-void LED_voidOn(u8 Copy_u8Port, u8 Copy_u8Pin);
-void LED_voidOff(u8 Copy_u8Port, u8 Copy_u8Pin);
-
-#endif
